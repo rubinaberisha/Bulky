03b1a662fa991c6e6f050e5e482e90a5241203db
rubinaberisha <rubinaberisha@gmail.com>
2025-01-10 16:50:02 +0100

    Add project files.

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,117 @@
+﻿using Bulky.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "It Ends with Us", 
+                    Author = "Colleen Hover",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    ISBN = "1471156265",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                },
+                   new Product
+                {
+                    Id = 2,
+                    Title = "The Summer I Turned Pretty", 
+                    Author = "Jenny Han",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    ISBN = "1416968296",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                },
+                   new Product
+                {
+                    Id = 3,
+                    Title = "A Good Girl's Guide to Murder", 
+                    Author = "Holly Jackson",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    ISBN = "9780593568477",
+                    ListPrice = 89,
+                    Price = 80,
+                    Price50 = 75,
+                    Price100 = 70,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                   },
+                    new Product
+                {
+                    Id = 4,
+                    Title = "Things we never got over", 
+                    Author = "Lucy Score",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    ISBN = "194563183X ",
+                    ListPrice = 69,
+                    Price = 60,
+                    Price50 = 55,
+                    Price100 = 50,
+                    CategoryId = 2,
+                    ImageUrl = ""
+                },
+                    new Product
+                {
+                    Id = 5,
+                    Title = "The Seven Husbands of Evelyn Hugo", 
+                    Author = "Taylor Jenkins Reid",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    ISBN = "1501139231",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                    CategoryId = 2,
+                    ImageUrl = ""
+                },
+                    new Product
+                {
+                    Id = 6,
+                    Title = "Reminders of Him", 
+                    Author = "Colleen Hover",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    ISBN = "1542025605",
+                    ListPrice = 59,
+                    Price = 50,
+                    Price50 = 45,
+                    Price100 = 40,
+                    CategoryId = 3,
+                    ImageUrl = ""
+                }
+                );
+        }
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20250103123148_addProductsToDb.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Colleen Hover", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "1471156265", 99.0, 90.0, 80.0, 85.0, "It Ends with Us" },
+                    { 2, "Jenny Han", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "1416968296", 40.0, 30.0, 20.0, 25.0, "The Summer I Turned Pretty" },
+                    { 3, "Holly Jackson", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "9780593568477", 89.0, 80.0, 70.0, 75.0, "A Good Girl's Guide to Murder" },
+                    { 4, "Lucy Score", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "194563183X ", 69.0, 60.0, 50.0, 55.0, "Things we never got over" },
+                    { 5, "Taylor Jenkins Reid", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "1501139231", 99.0, 90.0, 80.0, 85.0, "The Seven Husbands of Evelyn Hugo" },
+                    { 6, "Colleen Hover", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "1542025605", 59.0, 50.0, 40.0, 45.0, "Reminders of Him" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
